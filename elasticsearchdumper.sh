1a680e78a07f2299a69d0f0d9b9cc31a01dce731
F2N <benaou.pro@hotmail.com>
2017-03-31 12:05:40 +0100

    Update elasticsearchdumper.sh

--- elasticsearchdumper.sh
+++ elasticsearchdumper.sh
@@ -9,19 +9,19 @@ DAYS="2"
 
 EXPIREDATE=$(date -j -v-"$DAYS"d +"%Y.%m.%d");
 
-/usr/local/bin/elasticdump --input=http://192.168.1.111:9200/logstash-$EXPIREDATE --output=/tmp/logstach-$EXPIREDATE.json --quiet
+/usr/local/bin/elasticdump --input=http://YOUR-IP:9200/logstash-$EXPIREDATE --output=/tmp/logstach-$EXPIREDATE.json --quiet
 
 if [ $? -eq 0 ]
 	then
 	WCRESULT=($(/usr/bin/wc -l /tmp/logstach-$EXPIREDATE.json))
 	LINES=${WCRESULT=[0]}
-	LINESCURL=($(/usr/local/bin/curl "http://192.168.1.111:9200/logstash-$EXPIREDATE/_count" -s | egrep -o '[[:digit:]]+' | head -n1))
+	LINESCURL=($(/usr/local/bin/curl "http://YOUR-IP:9200/logstash-$EXPIREDATE/_count" -s | egrep -o '[[:digit:]]+' | head -n1))
 	#echo $LINES
 	#echo $LINESCURL
 	if [ $LINES -eq $LINESCURL ]
 	then
-		/usr/local/bin/curl -X POST http://192.168.1.111:9200/logstash-$EXPIREDATE/_close
-		/usr/local/bin/curl -X DELETE http://192.168.1.111:9200/logstash-$EXPIREDATE/_close
+		/usr/local/bin/curl -X POST http://YOUR-IP:9200/logstash-$EXPIREDATE/_close
+		/usr/local/bin/curl -X DELETE http://YOUR-IP:9200/logstash-$EXPIREDATE/_close
 			else
 		echo "Second condition went wrong"
 	fi
